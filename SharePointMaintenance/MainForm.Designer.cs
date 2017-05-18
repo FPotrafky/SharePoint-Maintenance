0b12637a8971237a7a87674310bf0ca5af481b73
f.potrafky@packof7.com <f.potrafky@packof7.com>
2017-05-18 21:58:39 +0200

    Removed old Solution

--- SharePointMaintenance/MainForm.Designer.cs
+++ SharePointMaintenance/MainForm.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("SiteCollection");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node0");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node0");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node1");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node2");
             this.contextMenuStripSiteCollections = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripMenuItemAddSiteCollection = new System.Windows.Forms.ToolStripMenuItem();
             this.mainTreeView = new System.Windows.Forms.TreeView();
@@ -42,33 +46,50 @@
             this.contextMenuStripSiteCollections.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItemAddSiteCollection});
             this.contextMenuStripSiteCollections.Name = "contextMenuStripSiteCollections";
-            this.contextMenuStripSiteCollections.Size = new System.Drawing.Size(342, 50);
+            this.contextMenuStripSiteCollections.Size = new System.Drawing.Size(173, 26);
             // 
             // toolStripMenuItemAddSiteCollection
             // 
             this.toolStripMenuItemAddSiteCollection.Name = "toolStripMenuItemAddSiteCollection";
-            this.toolStripMenuItemAddSiteCollection.Size = new System.Drawing.Size(341, 46);
+            this.toolStripMenuItemAddSiteCollection.Size = new System.Drawing.Size(172, 22);
             this.toolStripMenuItemAddSiteCollection.Text = "Add SiteCollection";
             // 
             // mainTreeView
             // 
-            this.mainTreeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mainTreeView.Location = new System.Drawing.Point(130, 174);
+            this.mainTreeView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mainTreeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainTreeView.Location = new System.Drawing.Point(49, 73);
+            this.mainTreeView.Margin = new System.Windows.Forms.Padding(1);
             this.mainTreeView.Name = "mainTreeView";
             treeNode1.ContextMenuStrip = this.contextMenuStripSiteCollections;
             treeNode1.Name = "SiteCollection";
             treeNode1.Text = "SiteCollection";
+            treeNode2.ContextMenuStrip = this.contextMenuStripSiteCollections;
+            treeNode2.Name = "Node0";
+            treeNode2.Text = "Node0";
+            treeNode3.Name = "Node0";
+            treeNode3.Text = "Node0";
+            treeNode4.Name = "Node1";
+            treeNode4.Text = "Node1";
+            treeNode5.Name = "Node2";
+            treeNode5.Text = "Node2";
             this.mainTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1});
-            this.mainTreeView.Size = new System.Drawing.Size(765, 410);
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5});
+            this.mainTreeView.Size = new System.Drawing.Size(289, 174);
             this.mainTreeView.TabIndex = 0;
+            this.mainTreeView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainTreeView_MouseDown);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1272, 1031);
+            this.ClientSize = new System.Drawing.Size(477, 432);
             this.Controls.Add(this.mainTreeView);
+            this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.contextMenuStripSiteCollections.ResumeLayout(false);
--- SharePointMaintenance/MainForm.cs
+++ SharePointMaintenance/MainForm.cs
@@ -16,5 +16,15 @@ namespace SharePointMaintenance
         {
             InitializeComponent();
         }
+
+        private void mainTreeView_MouseDown(object sender, MouseEventArgs e)
+        {
+            TreeNode clickedNode = this.mainTreeView.GetNodeAt(e.X, e.Y);
+            if (clickedNode.Bounds.Contains(e.X, e.Y))
+            {
+                this.mainTreeView.SelectedNode = clickedNode;
+            }
+        }
+
     }
 }
