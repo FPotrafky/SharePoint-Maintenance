16ee7fa58deeb6adca885807070b698d5140fdb4
f.potrafky@packof7.com <f.potrafky@packof7.com>
2017-06-06 13:57:18 +0200

    more

--- SPM.UI/Model/SPMList.cs
+++ SPM.UI/Model/SPMList.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SPM.Model
+namespace SPM.UI.Model
 {
-    public class SPM_List
+    class SPMList
     {
-        public int ID { get; set; }
+        public Guid ID { get; }
         public string Title { get; set; }
         public bool ShowInDisplayForm { get; set; }
         public bool ShowInNewForm { get; set; }
--- SPM.UI/Model/SPMSite.cs
+++ SPM.UI/Model/SPMSite.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SPM.DataAccess
+namespace SPM.UI.Model
 {
-    public class Class1
+    class SPMSite
     {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
     }
 }
